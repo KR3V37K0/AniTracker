d8fbc4ebc53587bb51e414e2d8e84c6ae31e0c06
KR3V37K0 <KR3V37K0@gmail.com>
2025-02-12 20:45:06 +0500

    Details-view screen design

--- Assets/Sc/UISC.cs
+++ Assets/Sc/UISC.cs
@@ -9,29 +9,42 @@ public class UISC : MonoBehaviour
     //NAVIGATION
     [SerializeField] GameObject[]icons=new GameObject[5];
     [SerializeField] GameObject[] windows = new GameObject[5];
+    [SerializeField] GameObject details;
     [SerializeField] GameObject selector;
     //[SerializeField] Dictionary<string, GameObject> icons = new Dictionary<string, GameObject>();
 
+    private void Start()
+    {   
+        details.SetActive(false);
+        activate_Window(0);
+    }
     public void activate_Window(int i)
     {
         //animation
-            //navigation
-            Tween.Scale(icons[int.Parse(selector.name)].transform, endValue: 1f, duration: 0.4f, endDelay: 0.1f);
-            Tween.PositionX(selector.transform, icons[i].transform.position.x, 0.25f);
-            Tween.Scale(icons[i].transform, endValue: 1.25f, duration:0.4f, endDelay: 0.1f);
-            selector.name = i.ToString();
-        //windows
-            foreach(GameObject go in windows)
-            {
-                go.SetActive(false);
-            }
-            windows[i].SetActive(true);
-
+            Sequence.Create(cycles: 1)
+                .Group(Tween.Scale(icons[int.Parse(selector.name)].transform, endValue: 1f, duration: 0.4f, endDelay: 0.1f))
+                .Group(Tween.PositionX(selector.transform, icons[i].transform.position.x, 0.25f))
+                .Group(Tween.Scale(icons[i].transform, endValue: 1.25f, duration: 0.4f, endDelay: 0.1f))
+                .ChainCallback(() => selector.name = i.ToString())
+                .ChainCallback(() => Swap_Windows(i));
+    }
+    private void Swap_Windows(int i)
+    {
+        foreach (GameObject go in windows)
+        {
+            go.SetActive(false);
+        }
+        windows[i].SetActive(true);
     }
     public void but_ViewDetails(GameObject i)
     {
         //animation
-            Tween.Scale(i.transform, 0.9f, 0.2f).OnComplete(()=>Tween.Scale(i.transform, 1f, 0.2f));
-            Tween.Color(i.transform.Find("poster/img").GetComponent<Image>(),Color.gray,0.2f).OnComplete(() => Tween.Color(i.transform.Find("poster/img").GetComponent<Image>(), Color.white, 0.2f));
+            Sequence.Create(cycles: 1)
+                .Group(Tween.Scale(i.transform, 0.9f, 0.2f))
+                .Group(Tween.Color(i.transform.Find("poster/img").GetComponent<Image>(), Color.gray, 0.2f))
+                .Chain(Tween.Scale(i.transform, 1f, 0.2f))
+                .Group(Tween.Color(i.transform.Find("poster/img").GetComponent<Image>(), Color.white, 0.2f))
+                .ChainCallback(() => details.SetActive(true))
+                .ChainCallback(() => activate_Window(0));
     }
 }
